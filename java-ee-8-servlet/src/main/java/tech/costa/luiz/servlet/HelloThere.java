8ef2181098c534adc01338cbf953e91e98f317d3
Luiz Gustavo <luiz.gustavo.costa82@gmail.com>
2020-09-15 23:32:01 +0200

    Some playground with servlet based on github

--- java-ee-8-servlet/src/main/java/tech/costa/luiz/servlet/HelloThere.java
+++ java-ee-8-servlet/src/main/java/tech/costa/luiz/servlet/HelloThere.java
@@ -1,6 +1,9 @@
 package tech.costa.luiz.servlet;
 
 import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
@@ -22,20 +25,47 @@ public class HelloThere extends HttpServlet {
         // TODO Auto-generated constructor stub
     }
 
-	/**
-	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
-	 */
-	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		response.getWriter().append("Served at: ").append(request.getContextPath());
-	}
+    @Override
+    public void doGet(HttpServletRequest request,
+            HttpServletResponse response)
+            throws ServletException, IOException {
 
-	/**
-	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
-	 */
-	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		doGet(request, response);
-	}
+        response.setContentType("text/html");
+        response.setBufferSize(8192);
+        try (PrintWriter out = response.getWriter()) {
+            out.println("<html lang=\"en\">"
+                    + "<head><title>Servlet Hello</title></head>");
+            
+            // then write the data of the response
+            out.println("<body  bgcolor=\"#ffffff\">"
+                    + "<img src=\"resources/images/duke.waving.gif\" "
+                    + "alt=\"Duke waving his hand\">"
+                    + "<form method=\"get\">"
+                    + "<h2>Hello, my name is Duke. What's yours?</h2>"
+                    + "<input title=\"My name is: \" type=\"text\" "
+                    + "name=\"username\" size=\"25\"/>"
+                    + "<p></p>"
+                    + "<input type=\"submit\" value=\"Submit\"/>"
+                    + "<input type=\"reset\" value=\"Reset\"/>"
+                    + "</form>");
+            
+            String username = request.getParameter("username");
+            if (username != null && username.length() > 0) {
+                RequestDispatcher dispatcher =
+                        getServletContext().getRequestDispatcher("/response");
+                
+                if (dispatcher != null) {
+                    dispatcher.include(request, response);
+                }
+            }
+            out.println("</body></html>");
+        }
+    }
+
+    @Override
+    public String getServletInfo() {
+        return "The Hello servlet says hello.";
+
+    }
 
 }
