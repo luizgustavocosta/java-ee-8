a46661fa1dbf52a1b7ea1d01ac6e7649a2b0098a
Luiz Gustavo <luiz.gustavo.costa82@gmail.com>
2020-10-04 15:42:31 +0200

    Initial commit

---
+++ ejb/ejbModule/tech/costa/luiz/ejb/service/CountVisitors.java
@@ -0,0 +1,20 @@
+package tech.costa.luiz.ejb.service;
+
+import javax.ejb.LocalBean;
+import javax.ejb.Singleton;
+
+/**
+ * Session Bean implementation class CountVisitors
+ */
+@Singleton
+@LocalBean
+public class CountVisitors {
+
+    /**
+     * Default constructor. 
+     */
+    public CountVisitors() {
+        // TODO Auto-generated constructor stub
+    }
+
+}
